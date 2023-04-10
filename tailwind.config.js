cb5be14029c1586d6527d780dbca6b883000f498
Butter on Bread <qorbaxk97@gmail.com>
2023-04-10 14:00:51 +0900

    [🚧] Initialize project settings

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#9000FF',
+        },
+        secondary: {
+          100: '#F7F7F9',
+          200: '#FFE8F5',
+        },
+        contentText: '#212121',
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+        tif: ['SBAggroB', 'sans-serif'],
+      },
+      maxWidth: {
+        scr: '31.25rem',
+      },
+    },
+  },
+  plugins: [],
+}
