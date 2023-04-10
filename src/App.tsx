a957a60c761c2549c7d1254e20836aff97bba6f5
Butter on Bread <qorbaxk97@gmail.com>
2023-04-10 14:27:08 +0900

    [:sparkles:] 기본 페이지 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Home, Question, Result } from './pack'
 
 function App() {
-  return <div>hi</div>
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/question" element={<Question/>}/>
+      <Route path="/result/:id" element={<Result/>}/>
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/pack.tsx
@@ -0,0 +1,3 @@
+export { default as Home } from './router/Home'
+export { default as Question } from './router/Question'
+export { default as Result } from './router/Result'
