e54da00f1ef8fe5eedffb85c5846a4cfac40b96a
Butter on Bread <qorbaxk97@gmail.com>
2023-04-10 15:34:19 +0900

    [:sparkles:] 입단하기 버튼 클릭시 질문페이지로 이동

--- src/router/Home.tsx
+++ src/router/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Cover from 'assets/maincover.gif'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+
   return (
     <section className="mx-auto max-w-scr p-4 flex flex-col min-h-screen h-full bg-secondary-100 items-center">
       <h1 className="bg-primary-100 text-white text-center text-4xl p-4 my-6 font-tif">
@@ -12,6 +15,7 @@ const Home = () => {
         role="button"
         aria-label="테스트 시작하기 버튼"
         className="p-4 border-2 border-primary-100 border-solid my-6 font-tif text-lg text-center min-w-[80%] hover:bg-primary-100 hover:text-secondary-100 transition-all duration-300"
+        onClick={() => navigate('/question')}
       >
         입단하기
       </button>
