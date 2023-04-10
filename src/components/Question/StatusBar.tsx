b68106542e7c1d92f06edcdfbaef75f65c065192
Butter on Bread <qorbaxk97@gmail.com>
2023-04-10 18:10:00 +0900

    [:lipstick:] 상태진행바 스타일링

--- src/components/Question/StatusBar.tsx
+++ src/components/Question/StatusBar.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from 'redux/store'
 
 const StatusBar = () => {
+  const endPoint = 12
+  const qIdx = useSelector((QueState: RootState) => QueState.qr.questionIndex)
+
+  const percent = (100 / endPoint) * (qIdx + 1)
+
   return (
-    <div>StatusBar</div>
+    <>
+      <span
+        role="progressbar"
+        aria-label="현재 진행상태"
+        className="text-xs font-bold"
+      >
+        {qIdx + 1}/12
+      </span>
+      <div aria-hidden className="w-full h-4 mx-auto bg-white">
+        <div
+          aria-hidden
+          className={`h-full bg-primary-100 w-[${percent}%]`}
+        ></div>
+      </div>
+    </>
   )
 }
 
