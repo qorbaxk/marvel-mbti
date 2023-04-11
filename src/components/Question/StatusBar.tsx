d8f19b4bea47d6a33483a9c1f569e889c5cea002
Butter on Bread <qorbaxk97@gmail.com>
2023-04-11 14:22:49 +0900

    [:lady_beetle:] 진행바 클래스 동적 할당 되지 않던 버그 수정

--- src/components/Question/StatusBar.tsx
+++ src/components/Question/StatusBar.tsx
@@ -3,13 +3,27 @@ import { useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
 
 const StatusBar = () => {
-  const [progress, setProgress] = useState(8.3)
+  const [progress, setProgress] = useState<string>('w-[8.3%]')
+  const percent = [
+    'w-[8.3%]',
+    'w-[16.6%]',
+    'w-[24.9%]',
+    'w-[33.2%]',
+    'w-[41.5%]',
+    'w-[49.8%]',
+    'w-[58.1%]',
+    'w-[66.4%]',
+    'w-[74.7%]',
+    'w-[83%]',
+    'w-[91.3%]',
+    'w-[100%]',
+  ]
   const endPoint = 12
   const qIdx = useSelector((QueState: RootState) => QueState.qr.questionIndex)
 
   useEffect(() => {
     if (qIdx !== endPoint) {
-      setProgress(Math.floor((100 / endPoint) * (qIdx + 1)))
+      setProgress(percent[qIdx])
     }
   }, [qIdx])
 
@@ -23,10 +37,7 @@ const StatusBar = () => {
         {qIdx + 1}/12
       </span>
       <div aria-hidden className="w-full h-4 mx-auto bg-white">
-        <div
-          aria-hidden
-          className={`w-[${progress}%] h-full bg-primary-100 `}
-        ></div>
+        <div aria-hidden className={`h-full bg-primary-100 ${progress}`}></div>
       </div>
     </>
   )
