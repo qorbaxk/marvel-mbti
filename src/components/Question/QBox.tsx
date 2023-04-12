6ead7b5288ed7714e7b1c245606378142ceb5c9d
Baxk <qorbaxk97@gmail.com>
2023-04-12 16:19:29 +0900

    Revert "디벨롭 브랜치 배포를 위한 피알"

--- src/components/Question/QBox.tsx
+++ src/components/Question/QBox.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
+import { qnaList } from 'data/data'
 import { useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
-import { qnaList } from 'data/data'
 
 const QBox: React.FC = () => {
   const qIdx = useSelector((QueState: RootState) => QueState.qr.questionIndex)
 
   return (
     <>
-      <h2
-        aria-label="질문"
-        className="w-full p-4 my-10 bg-primary-100 text-white text-2xl text-center grow flex items-center"
-      >
+      <h2 className="w-full p-4 my-10 bg-primary-100 text-white text-2xl text-center grow flex items-center">
         {qnaList[qIdx]?.q}
       </h2>
     </>
